738ef8cb6f64c108d50648b39306b8fb705c5404
Juan Cassiano <juancassiano@hotmail.com>
2022-11-06 17:59:06 -0300

    Create Detail Product component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  { path: '', redirectTo: 'produtos', pathMatch: "full"}
+  { path: '', redirectTo: 'produtos', pathMatch: "full"},
+  { path: '**', component: NaoEncontradoComponent},
 ];
 
 @NgModule({
